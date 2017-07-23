efd69acf86136cf0f7e8173ceeca1227dbb99f69
Noud Aldenhoven <noud.aldenhoven@gmail.com>
2017-07-23 21:52:44 +0200

    Better alignment.

--- personalized_medicine/load_data.py
+++ personalized_medicine/load_data.py
@@ -9,6 +9,20 @@ training_text_file = os.path.join(data_dir, 'training_text')
 training_variants_file = os.path.join(data_dir, 'training_variants')
 
 train_variants_df = pd.read_csv(training_variants_file)
-train_text_df = pd.read_csv(training_text_file, sep = '\|\|', engine = 'python', header = None, skiprows = 1, names = ['ID', 'Text'])
+train_text_df = pd.read_csv(
+    training_text_file,
+    sep = '\|\|',
+    engine = 'python',
+    header = None,
+    skiprows = 1,
+    names = ['ID', 'Text']
+)
 test_variants_df = pd.read_csv(test_variants_file)
-test_text_df = pd.read_csv(test_text_file, sep = '\|\|', engine = 'python', header = None, skiprows = 1, names = ['ID', 'Text'])
+test_text_df = pd.read_csv(
+    test_text_file,
+    sep = '\|\|',
+    engine = 'python',
+    header = None,
+    skiprows = 1,
+    names = ['ID', 'Text']
+)
