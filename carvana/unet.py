378ab5977cf0678a4cfe49d8baedd208a6c95e6a
Noud Aldenhoven <noud.aldenhoven@gmail.com>
2017-09-09 14:33:21 +0200

    Add smaller unet for speed.

--- carvana/unet.py
+++ carvana/unet.py
@@ -13,6 +13,58 @@ class conv_block(nn.Module):
     def forward(self, x):
         return self.relu(self.bn(self.conv(x)))
 
+class unet_256_small(nn.Module):
+    def __init__(self):
+        super(unet_256_small, self).__init__()
+
+        self.down_1 = nn.Sequential(
+            conv_block(3, 16),
+            conv_block(16, 32, stride = 2, padding = 1)
+        )
+
+        self.down_2 = nn.Sequential(
+            conv_block(32, 64),
+            conv_block(64, 128)
+        )
+
+        self.middle = conv_block(128, 128, kernel_size = 1, padding = 0)
+
+        self.up_2 = nn.Sequential(
+            conv_block(256, 128),
+            conv_block(128, 32)
+        )
+
+        self.up_1 = nn.Sequential(
+            conv_block(64, 64),
+            conv_block(64, 32)
+        )
+
+        self.output = nn.Sequential(
+            conv_block(32, 16),
+            conv_block(16, 1, kernel_size = 1, padding = 0)
+        )
+
+    def forward(self, x):
+        down1 = self.down_1(x)
+        out = F.max_pool2d(down1, kernel_size = 2, stride = 2)
+
+        down2 = self.down_2(out)
+        out = F.max_pool2d(down2, kernel_size = 2, stride = 2)
+
+        out = self.middle(out)
+
+        out = F.upsample_bilinear(out, scale_factor = 2)
+        out = torch.cat([down2, out], 1)
+        out = self.up_2(out)
+
+        out = F.upsample_bilinear(out, scale_factor = 2)
+        out = torch.cat([down1, out], 1)
+        out = self.up_1(out)
+
+        out = F.upsample_bilinear(out, scale_factor = 2)
+
+        return self.output(out)
+
 class unet_256(nn.Module):
     '''Input 256 x 256 image'''
     def __init__(self):
