8f25268bbb420772240b4e0bab6a9b0ab50563e3
Noud Aldenhoven <noud.aldenhoven@gmail.com>
2018-02-03 09:13:23 +0100

    Convert to numpy array.

--- toxic/doc2vec/convert.py
+++ toxic/doc2vec/convert.py
@@ -2,6 +2,8 @@
 
 import gensim
 
+import numpy as np
+
 from train import tokenizer
 
 class DocToVec(object):
@@ -10,3 +12,6 @@ class DocToVec(object):
 
     def __call__(self, inp):
         return [self.model[w] for s in tokenizer(inp) for w in s]
+
+    def toNumpy(self, inp):
+        return np.expand_dims(np.array(self(inp)), 0)
